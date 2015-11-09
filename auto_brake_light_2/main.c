d3722a9e16cab9b4dff4f32102be48bdaf34bd90
stolmen <edward93@gmail.com>
2015-11-09 21:45:08 +1100

    Working implementation of slow pitch compensation. Needs calibration.

--- auto_brake_light_2/main.c
+++ auto_brake_light_2/main.c
@@ -8,8 +8,9 @@
 #define K_FACTOR 0.1f
 
 // State machine coefficients
-#define THRESH ((int) 0)
+#define THRESH 2000
 
+#define ACCEL_SENS_F 16384.0f		// ACCELEROMETER SENSITIVITY FLOAT
 
 // Initial accelerations!
 // Divide by 16
@@ -23,6 +24,7 @@ static void allLEDOff();
 static void allLEDOn();
 static void readAccel(accel_data *data);
 
+char update_pitch;
 
 /*
  * main.c
@@ -82,14 +84,23 @@ int main(void) {
 
 	allLEDOff();
 
-	char state = 1;
-
 	// Kill all interrupts.
 	_BIC_SR(GIE);
 
 	// Clear P1IFG and release MPU6050 int_status latch
 	P1IFG = 0;
 	iicRead(MPU6050_INT_STATUS);
+
+	// Declare some vars
+	char state = 1;
+	int comp_z;
+	int cur_z = 0;
+	update_pitch=0;
+
+	// Timer setup
+	CCTL0 = CCIE;                             // CCR0 interrupt enabled
+	CCR0 = 50000;
+	TACTL = TASSEL_2 + MC_2 + ID_3;                  // SMCLK, contmode, /8
 
 	// Disable all maskable interrupts, THEN un-mask interrupts on ACCEL_INT.
 	// This prevents jumping into the ISR immediately after un-masking.
@@ -98,17 +109,42 @@ int main(void) {
 	P1IE |= ACCEL_INT;		// enable interrupts
 
 	// Begin primary function state machine.
+	/*
+	 * The flow:
+	 * 1. Update accelerometer reading variables
+	 * 2. Perform pitch compensation on z (this only happens every now and then)
+	 * 3. Parse compensated z into state machine
+	 * 4. Quick LPF
+	 * 4. LEDs will light up depending on the state
+	 * 5. Wait for more accelerometer data in LPM3. Then repeat!
+	 */
 	while(1){
 		_BIS_SR(LPM3_bits + GIE);
-
-		readAccel(&current_accel);
 
 		// Kill all interrupts.
 		_BIC_SR(GIE);
 
+		readAccel(&current_accel);
+
+		if (update_pitch){
+			/*
+			 * Periodic pitch compensation.
+			 * 1. Calculate current compensation amount from z accel reading.
+			 * 2. Update compensation amount through averaging.
+			 * 3. Reset update_pitch.
+			 */
+
+			comp_z = (comp_z/16*15) + (current_accel.z/16);
+
+			update_pitch = 0;
+		}
+
+		current_accel.z = current_accel.z - comp_z;
+		cur_z = cur_z/8*7 + current_accel.z/8;
+
 		// set new state.
 		// Hysteresis!
-		if (current_accel.z > 0){
+		if (abs(cur_z) > THRESH){
 			state = 1;
 		} else {
 			state = 2;
@@ -117,14 +153,10 @@ int main(void) {
 		// Only wakes up when interrupt is received from PORT 1.
 		switch(state){
 		case 1:
-			// All LEDs on!
-			P1OUT |= LED2_PIN + LED4_PIN;
-			P1OUT &= ~(LED1_PIN + LED3_PIN);
+			allLEDOn();
 			break;
 		case 2:
-			// Turn all LEDs off!
-			P1OUT |= LED1_PIN + LED3_PIN;
-			P1OUT &= ~(LED2_PIN + LED4_PIN);
+			allLEDOff();
 			break;
 		}
 
@@ -139,37 +171,64 @@ int main(void) {
 
 
 static void allLEDOff(){
-	P1OUT |= LED1_PIN + LED3_PIN;
+	//P1OUT |= LED1_PIN + LED3_PIN;
 	P1OUT &= ~(LED2_PIN + LED4_PIN);
 }
 static void allLEDOn(){
 	P1OUT |= LED2_PIN + LED4_PIN;
-	P1OUT &= ~(LED1_PIN + LED3_PIN);
+	//P1OUT &= ~(LED1_PIN + LED3_PIN);
 }
 
-
+/*
+ * readAccel
+ * Updates 'data' struct with new accel readings.
+ * Flow (for each of x,y,z):
+ * 1. Read 2 bytes into memory
+ * 2. Construct 2 byte word and store into struct
+ */
 static void readAccel(accel_data *data){
-	// Read and construct 16-bit data
 	char x0,x1,y0,y1,z0,z1;
 
 	z0 = iicRead(MPU6050_ACCEL_ZOUT_L);
 	z1 = iicRead(MPU6050_ACCEL_ZOUT_H);
 	data->z = z0 + (z1 << 8);
-
+	/*
 	y0 = iicRead(MPU6050_ACCEL_YOUT_L);
 	y1 = iicRead(MPU6050_ACCEL_YOUT_H);
 	data->y = y0 + (y1 << 8);
 
 	x0 = iicRead(MPU6050_ACCEL_XOUT_L);
 	x1 = iicRead(MPU6050_ACCEL_XOUT_H);
-	data->x = x0 + (x1 << 8);
+	data->x = x0 + (x1 << 8);*/
 }
 
-// interrupts from GPIO
+/*
+ * Port 1 ISR
+ * Waking up from this will get the accelerometer readings updated!
+ * Flow:
+ * 1. Mask all P1 interrupts and clear interrupt flag
+ * 2. Reset the update_pitch flag to signal accelerometer reading!
+ * 3. (Accelerometer readings are updated in the state machine)
+ */
 #pragma vector=PORT1_VECTOR
 __interrupt void PORT1 (void){
 	// Disable interrupt until we're done processing the current data.
 	P1IFG &= ~ACCEL_INT;
 	P1IE &= ~ACCEL_INT;
+	_BIC_SR(LPM3_EXIT); // wake up from low power mode
+}
+
+/*
+ * Timer A0 ISR
+ * Waking up from this will get the pitch updated!
+ * Flow:
+ * 1. Set the flag
+ * 2. Reset timer
+ * 4. (Pitch will get updated in state machine next time it wakes up.)
+ */
+#pragma vector=TIMERA0_VECTOR
+__interrupt void TIMERA0(void){
+	update_pitch = 1;
+	P1OUT ^= LED1_PIN;
 	_BIC_SR(LPM3_EXIT); // wake up from low power mode
 }
